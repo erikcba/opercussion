139ab5e305399ea7d48ea3deaeb19b454e45d2bc
Erik Mathias Tomaselli <eriktomaselli97@gmail.com>
2024-11-07 11:26:44 -0300

    agregado de traduccion, faltan textos (branch multilenguaje)

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,8 +2,10 @@ import esquinaIzq from '../assets/esquinaIzq.png'
 import esquinaDer from '../assets/esquinaDer.png'
 import ReadMore from '../components/ReadMore'
 import Members from './Members'
+import { useTranslation } from 'react-i18next'
 
 const About = () => {
+  const { t } = useTranslation()
   return (
     <section id="about" className='sm:container container flex flex-column sm:gap-5 gap-2 xl:px-32 '>
       <div className='flex flex-row'>
@@ -15,7 +17,7 @@ const About = () => {
             <h1 className='sm:text-6xl text-4xl font-light text-center'>Art</h1>
           </div>
           <p className='sm:text-3xl text-xl font-normal text-center sm:leading-10 leading-8'>
-            Opercussion is a one-of-a-kind ensemble that extracts the percussion section from the depths of the orchestra pit and thrusts it centre stage, by blending rich music history with daring innovation.
+            {t('aboutTitle')}
           </p>
         </div>
         <img src={esquinaDer} alt="esquinaDer" className='h-1/2 self-end sm:block hidden' />
