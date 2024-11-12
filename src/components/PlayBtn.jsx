bef1a00bfe52a7796d77cf738cf72864016d5bd0
Erik Mathias Tomaselli <eriktomaselli97@gmail.com>
2024-11-12 11:16:53 -0300

    prueba de boton listen now mobile

--- src/components/PlayBtn.jsx
+++ src/components/PlayBtn.jsx
@@ -3,10 +3,14 @@ import playbtnViolet from '../assets/playbtnViolet.png'
 
 const PlayBtn = () => {
   return (
-    <div className='fixed lg:bottom-20 bottom-10 lg:right-24 right-10 z-50 flex flex-col items-center justify-center gap-2 hover:cursor-pointer'>
-      <img className='h-min w-min' src={playbtnViolet} alt="" />
-      <p className='uppercase text-white bg-fuchsia-800 rounded-full py-2 px-4 leading-none text-sm font-semibold'>Listen to us!</p>
-    </div>
+    <>
+    <div className='forma'>&nbsp;</div>
+    <div className='forma2'>&nbsp;</div>
+      <div className='fixed lg:bottom-20 bottom-10 lg:right-24 right-10 z-50 flex flex-col items-center justify-center gap-2 hover:cursor-pointer'>
+        <img className='h-min w-min' src={playbtnViolet} alt="" />
+        <p className='uppercase text-white bg-fuchsia-800 rounded-full py-2 px-4 leading-none text-sm font-semibold'>Listen to us!</p>
+      </div>
+    </>
   )
 }
 
